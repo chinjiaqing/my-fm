7b970124abbb341c027d13990311d4aab65b5e76
chinjiaqing <chinjiaqing@qq.com>
2025-03-31 22:49:09 +0800

    fix: base prefix path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-fm/' : '/',
   plugins: [
       vue(),
       UnoCSS()
