f9cd4ca3540cc768310eaf5b8b90e248f2e36190
chinjiaqing <chinjiaqing@qq.com>
2025-04-01 14:50:26 +0800

    feature: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,40 @@ import vue from '@vitejs/plugin-vue'
 import px2viewport from "postcss-px-to-viewport-8-plugin"
 import UnoCSS from "unocss/vite"
 import path from 'path'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
     base: process.env.NODE_ENV === 'production' ? '/my-fm/' : '/',
     plugins: [
         vue(),
-        UnoCSS()
+        UnoCSS(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'MY FM | 我的收音机',
+                short_name: "MY FM",
+                description: '简单听一听',
+                theme_color: '#7f64f8',
+                icons: [
+                    {
+                        src: 'fm.png',
+                        sizes: '625x625',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
     ],
-    server:{
-        host:'0.0.0.0',
+    server: {
+        host: '0.0.0.0',
     },
-    resolve:{
-        alias:{
-            '@':path.resolve(__dirname,'./src')
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
         }
     },
-    css:{
+    css: {
         postcss: {
             plugins: [
                 px2viewport({
